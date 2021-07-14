eb9508b1a73bb36fec71fabe88b692c270ba70a8
thangta881999 <thangta881999@gmail.com>
2021-07-15 06:54:48 +0700

    thangnew3

--- src/main/java/com/TP/chatbot/ChatBotApplication.java
+++ src/main/java/com/TP/chatbot/ChatBotApplication.java
@@ -22,19 +22,20 @@ public class ChatBotApplication {
      * @param PAGE_ACCESS_TOKEN the generated {@code Page Access Token}
      */
 
-//    @Bean
-//    //@Autowired
-//    public MessengerSendClient messengerSendClient(@Value("EAADHFIeBIwABAKPCKfhawzZCyBqS4FrGjC1sUkKCxZCOB1hZBuiP38HOEemBZCKWjIom4sDcHrdvvp5rJWGcBw7CihAuxUY045ygdTjnX8DzQ32JSZAEBoWScwsBEs2mNm5pQ6uNFXD5tSU1i1QXSmCZBWV9yAfNgeDNKS3Mz55CdZCYjNJR4X7QL2xrXO5vSgZD") String PAGE_ACCESS_TOKEN,
-//                                                   @Value("5b151aac813b854bdacb026ae9c31772") final String APPSECRET,
-//                                                   @Value("123456789") final String VERIFY_TOKEN) {
-//        logger.debug("Initializing MessengerSendClient - PAGE_ACCESS_TOKEN: {}", PAGE_ACCESS_TOKEN,APPSECRET,VERIFY_TOKEN);
-//        return MessengerPlatform.newSendClientBuilder(PAGE_ACCESS_TOKEN).build();
-//    }
-
     @Bean
     @RequestMapping
-    public MessengerSendClient messengerSendClient(@Value("EAACktZAjdwbUBAIA7hnUSAr6XWdGRpIZAOKvx7eoFfnRlEa64Is6bYeJdfcrZBlruTsdUTJK7exn3Ckg2tYkMbbyZARKTWnWHTf37BCjHh6v3GkFhDwkWpMa86Ee1rVExzZC1QqZBBZAb8Ul8OZBRgXTw1X0qtIZC4zi01WJjFtsJclnrMCByhBG12dIzZBEqXNg8ZD") String PAGE_ACCESS_TOKEN) {
-        logger.debug("Initializing MessengerSendClient - PAGE_ACCESS_TOKEN: {}", PAGE_ACCESS_TOKEN);
+    @Autowired
+    public MessengerSendClient messengerSendClient(@Value("EAACktZAjdwbUBAIA7hnUSAr6XWdGRpIZAOKvx7eoFfnRlEa64Is6bYeJdfcrZBlruTsdUTJK7exn3Ckg2tYkMbbyZARKTWnWHTf37BCjHh6v3GkFhDwkWpMa86Ee1rVExzZC1QqZBBZAb8Ul8OZBRgXTw1X0qtIZC4zi01WJjFtsJclnrMCByhBG12dIzZBEqXNg8ZD") String PAGE_ACCESS_TOKEN,
+                                                   @Value("f8d3af2e47cfd338a64541f9ea0aa308") final String APPSECRET,
+                                                   @Value("123456789") final String VERIFY_TOKEN) {
+        logger.debug("Initializing MessengerSendClient - PAGE_ACCESS_TOKEN: {}", PAGE_ACCESS_TOKEN,APPSECRET,VERIFY_TOKEN);
         return MessengerPlatform.newSendClientBuilder(PAGE_ACCESS_TOKEN).build();
     }
+
+//    @Bean
+//    @RequestMapping
+//    public MessengerSendClient messengerSendClient(@Value("EAACktZAjdwbUBAIA7hnUSAr6XWdGRpIZAOKvx7eoFfnRlEa64Is6bYeJdfcrZBlruTsdUTJK7exn3Ckg2tYkMbbyZARKTWnWHTf37BCjHh6v3GkFhDwkWpMa86Ee1rVExzZC1QqZBBZAb8Ul8OZBRgXTw1X0qtIZC4zi01WJjFtsJclnrMCByhBG12dIzZBEqXNg8ZD") String PAGE_ACCESS_TOKEN) {
+//        logger.debug("Initializing MessengerSendClient - PAGE_ACCESS_TOKEN: {}", PAGE_ACCESS_TOKEN);
+//        return MessengerPlatform.newSendClientBuilder(PAGE_ACCESS_TOKEN).build();
+//    }
 }
