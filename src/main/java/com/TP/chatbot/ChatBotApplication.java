70f9d496a7b496a055d78a4669fe00754ae805ec
thangta881999 <thangta881999@gmail.com>
2021-07-17 08:18:26 +0700

    last1

--- src/main/java/com/TP/chatbot/ChatBotApplication.java
+++ src/main/java/com/TP/chatbot/ChatBotApplication.java
@@ -16,7 +16,7 @@ import com.github.messenger4j.Messenger;
 @Configuration
 public class ChatBotApplication {
     @Bean
-    public Messenger messenger(@Value("EAACktZAjdwbUBAIGZBtgmOQBjWhN5QHuOmI6FD9dyvxS6Yr735vGeIS5SNtauIpX0A54Cof45cZBAEywq6XZBMi1iZAcOwZBuZAKaZAIqyJv7HgNjr6TrMXLTFAxjLAeW2blUyhGCYZAjN9qlZCRjAkiXtfzpRtSdvXwnQwEzofkqcnjbDHZCfYMnjY863JuEZAQgwYZD")  String PAGE_ACCESS_TOKEN,
+    public Messenger messenger(@Value("EAACktZAjdwbUBAIcufCNiZAGIERhhrH0ce5DRjqpb03tvzIXABZCp614YkFgvZBgJCIjPYou8bG8UH2p1TCGPdHZCZCRXxZBtfZCLu3EKdeYbbIGpQC5tkiXhkIbEIvuOHoWZBtFHBhkZAMkykkaEBpKqL3CPtisXt6POzvxYqZCrQ0DOC8oSM7pWOWVYC2aj30sRkZD")  String PAGE_ACCESS_TOKEN,
                                @Value("f8d3af2e47cfd338a64541f9ea0aa308")  final String APPSECRET,
                                @Value("$123456789") final String VERIFY_TOKEN) {
         return Messenger.create(PAGE_ACCESS_TOKEN, APPSECRET, VERIFY_TOKEN);
